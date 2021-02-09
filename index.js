3b5dbd5679c79c5a85fbae256bcd22c28d934004
habeshawit <shalomseleshi@gmail.com>
2021-02-09 06:41:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+    }
+    return arr;
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int--;
+    }
+}
